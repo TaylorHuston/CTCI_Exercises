694ef3616a27410d1b34b5806df2ca4d4b0e9af6
Taylor Huston <taylor.a.huston@gmail.com>
2015-10-14 23:10:39 -0700

    Add traversals

--- Chapter4/BinaryFromArray.java
+++ Chapter4/BinaryFromArray.java
@@ -1,9 +1,10 @@
 package Chapter4;
 
+
 /*Build a BST from an already sorted array*/
 public class BinaryFromArray {
 
-    static class Node {
+    public static class Node {
         public int id;
         public Node left;
         public Node right;
@@ -11,6 +12,34 @@ public class BinaryFromArray {
         Node(int id) {
            this.id = id;
         }
+
+        public static void inOrder(Node head) {
+            if (head == null) {
+                return;
+            }
+            inOrder(head.left);
+            System.out.print(head.id + " ");
+            inOrder(head.right);
+        }
+
+        public static void preOrder(Node head) {
+            if (head == null) {
+                return;
+            }
+            System.out.print(head.id + " ");
+            preOrder(head.left);
+            preOrder(head.right);
+        }
+
+        public static void postOrder(Node head) {
+            if (head == null) {
+                return;
+            }
+            postOrder(head.left);
+            postOrder(head.right);
+            System.out.print(head.id + " ");
+        }
+
     }
 
     public static Node buildTree(int[] arr) {
@@ -34,5 +63,14 @@ public class BinaryFromArray {
             sorted[i] = i;
         }
         Node myBST = buildTree(sorted);
+
+        Node.inOrder(myBST);
+        System.out.println();
+
+        Node.preOrder(myBST);
+        System.out.println();
+
+        Node.postOrder(myBST);
+        System.out.println();
     }
 }
